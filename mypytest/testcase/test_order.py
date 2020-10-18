f926ff58d1eef08f9d1f6ad8db2fd45d78816c9e
fanpengli <fanpengli@fangdd.com>
2020-10-18 10:46:14 +0800

    pytest 执行顺序order练习

---
+++ mypytest/testcase/test_order.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/18 10:30
+# @Author  : 饭盆里
+# @File    : test_order.py
+# @Software: PyCharm
+# @desc    : 练习pytest运行顺序，需要提前安装order
+import pytest
+
+
+class TestOrder():
+    def test_case1(self):
+        print('test_case1')
+
+    @pytest.mark.last
+    def test_case2(self):
+        print('test_case2')
+
+    def test_case3(self):
+        print('test_case3')
+
+    @pytest.mark.run(order=1)
+    def test_case4(self):
+        print('test_case4')
