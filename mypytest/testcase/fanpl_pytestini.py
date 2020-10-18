b8c2ad1bd19902e584caaaee17d4a09e004f3231
fanpengli <fanpengli@fangdd.com>
2020-10-18 18:18:01 +0800

    pytest pytest.ini 修改运行规则

---
+++ mypytest/testcase/fanpl_pytestini.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/18 18:14
+# @Author  : 饭盆里
+# @File    : fanpl_pytestini.py
+# @Software: PyCharm
+# @desc    : 测试pytest.ini配置文件   (可以参考 pytest --help 中的配置说明)
+
+class Fanpl_pytestini():
+    def fan_test(self):
+        print('hi')
