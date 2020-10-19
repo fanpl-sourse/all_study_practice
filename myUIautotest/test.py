c60386387a4f1d1a89055ea74c82ffb2078a3265
fanpengli <fanpengli@fangdd.com>
2020-10-19 15:39:27 +0800

    开启UI自动化测试之旅

---
+++ myUIautotest/test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/19 15:31
+# @Author  : 饭盆里
+# @File    : test.py
+# @Software: PyCharm
+# @desc    : 验证一下环境
+
+from selenium import webdriver
+from time import sleep
+
+
+driver = webdriver.Chrome()
+driver.get('https://www.baidu.com/')
+sleep(2)
+driver.quit()
